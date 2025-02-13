d2a75593bb861c729eaf503da22b8c9db291514c
Skelett0101 <crisblas08@gmail.com>
2025-02-12 18:51:26 -0600

    'CUARTA__UP'

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,7 +1,32 @@
+
+from Usuario import Usuario
+
 class Aplicacion:
     def __init__(Aplicacion):
         Aplicacion.usuarios = {}  # Diccionario de usuarios registrados
        # Aplicacion.pago = Pago()  # Instancia de la clase pago
+       
+       
+    
+    def registrar_usuario(Aplicacion, id_usuario, nombre, numero_cuenta, nip, saldo_inicial=0):
+        """ Registra un nuevo usuario en el sistema. """
+        if numero_cuenta in Aplicacion.usuarios:
+            print("Error: La cuenta ya existe.")
+            return
+        Aplicacion.usuarios[numero_cuenta] = Usuario(id_usuario, nombre, numero_cuenta, nip, saldo_inicial)
+        print(f"Usuario {nombre} registrado con éxito.")
+
+    def iniciar_sesion(Aplicacion):
+        
+        """ Permite al usuario ingresar su número de cuenta y NIP para iniciar sesión. """
+        numero_cuenta = input("Ingrese su número de cuenta: ")
+        nip = input("Ingrese su NIP: ")
+
+        if numero_cuenta in Aplicacion.usuarios and Aplicacion.usuarios[numero_cuenta].iniciar_sesion(nip):
+            print("Inicio de sesión exitoso.")
+            Aplicacion.mostrar_menu(Aplicacion.usuarios[numero_cuenta])
+        else:
+            print("Número de cuenta o NIP incorrecto.")
    
    
     def menuI(Aplicacion):
@@ -26,9 +51,19 @@ class Aplicacion:
                 
                 nombre = input("Ingresa tu nombre: ")
                 numero_cu = input("Ingresa tu numero de cuenta: ")
-                niip = input("Ingresa tu nip: ")
-                saldo = 1000
                 
+                try:
+                    
+                    niip = int(input("Ingresa tu nip: "))
+                    
+                except ValueError:
+                    
+                    print("-------------------------------------------------------------------------------")
+                    print("Ingresa una opción válida de solo numeros.")
+
+
+                saldo = 1000
+                # len(Aplicacion.usuarios) + 1 / sirve para auto incrementar la id
                 Aplicacion.registrar_usuario(len(Aplicacion.usuarios) + 1,nombre,numero_cu,niip,saldo)
                 
             else:
@@ -36,4 +71,3 @@ class Aplicacion:
                 print("-------------------------------------------------------------------------------")
                 
                 print("Selecciona solo los numeros mostrados")
-                
---
+++ Usuario.py
@@ -0,0 +1,13 @@
+from Cuenta import Cuenta
+
+class Usuario:
+    def __init__(Usuario, id_usuario, nombre, numero_cuenta, nip, saldo=0):
+        Usuario.id_usuario = id_usuario
+        Usuario.nombre = nombre
+        Usuario.nip = nip
+        Usuario.cuenta = Cuenta(numero_cuenta, saldo)
+        Usuario.contactos = {}  # Diccionario de contactos para realizar transferencias
+        
+    def iniciar_sesion(Usuario, nip_ingresado):
+        """ Verifica que el NIP ingresado sea correcto. """
+        return Usuario.nip == nip_ingresado
