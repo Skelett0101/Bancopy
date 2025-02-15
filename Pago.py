995507d5233fdb2d2c5b3eb45df1cfffa8a61a32
Christian <scottpttrs@gmail.com>
2025-02-14 20:32:21 -0600

    'Chris_up1'

---
+++ Pago.py
@@ -0,0 +1,12 @@
+class Pago:
+
+    def __init__ (pago):
+
+        pago.pagos_pendientes = []
+
+    def generar_pago(pago, usuario_origen, monto, usuario_destino):
+
+
+        
+
+    def cobrar_pago(pago, usuario_destino, codigo):
